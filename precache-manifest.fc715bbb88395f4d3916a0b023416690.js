b34358dfa9392f81885a3b464dd8758a180bd32c
arjun dev pk <cb.en.u4cse18208@cb.students.amrita.edu>
2021-01-10 23:21:16 +0530

    Updates

---
+++ precache-manifest.fc715bbb88395f4d3916a0b023416690.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f3c9e1ef88c5ed9493b90c8f792e89",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ea323089be5233e28ba4",
+    "url": "/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "343990843710a6129517",
+    "url": "/portfolio/static/css/main.dee4a686.chunk.css"
+  },
+  {
+    "revision": "ea323089be5233e28ba4",
+    "url": "/portfolio/static/js/2.c2f8ec26.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.c2f8ec26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "343990843710a6129517",
+    "url": "/portfolio/static/js/main.c9b31199.chunk.js"
+  },
+  {
+    "revision": "976dd82999372d9d892d",
+    "url": "/portfolio/static/js/runtime-main.5676a5cc.js"
+  },
+  {
+    "revision": "14267b35632b9d1ad96611b178c827a6",
+    "url": "/portfolio/static/media/logo.14267b35.png"
+  },
+  {
+    "revision": "2f6663ba1a4bb42fa69d170cab9f828e",
+    "url": "/portfolio/static/media/me.2f6663ba.jpg"
+  },
+  {
+    "revision": "32ffa4fc34f3a4eb7934c600ea45ebd1",
+    "url": "/portfolio/static/media/resume.32ffa4fc.pdf"
+  }
+]);
