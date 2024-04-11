5fbba2f63793d408a1b4634055a3b525f6777a69
Arjun2001 <arjunsmart2001@gmail.com>
2024-04-11 18:49:21 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.748138a5783d58d07c122087d1aa3efb.js"
+  "/portfolio/precache-manifest.0ff22d360a6d76cd184a002d49cc999e.js"
 );
 
 self.addEventListener('message', (event) => {
